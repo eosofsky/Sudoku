34cefb2834b18c0a360b170680e3f6e6ce0ea61f
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-02-06 21:50:16 -0500

    Rotate camera.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	private Camera camera;
+	private GameObject light;
+
+	void Start () {
+		camera = GetComponent<Camera> ();
+		light = GameObject.FindGameObjectWithTag ("Light");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			StartCoroutine (rotateCamera (false));
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			StartCoroutine (rotateCamera (true));
+		}
+	}
+
+	IEnumerator	rotateCamera (bool left) {
+		int amount = 10;
+		Vector3 dir = left ? Vector3.up : -Vector3.up;
+		for (int i = 0; i < 90 / amount; i++) {
+			camera.transform.RotateAround (Vector3.zero, dir, amount);
+			light.transform.RotateAround (Vector3.zero, dir, amount);
+			yield return new WaitForSeconds (0.05f);
+		}
+		yield return null;
+	}
+}
