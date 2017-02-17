99acd7d49d19db45894ab389ea4c34501a1f216e
bswalsh <bswalsh@andrew.cmu.edu>
2017-02-16 20:51:07 -0500

    added aesthetic stuff to cube

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,22 +6,35 @@ using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     public GameObject cube;
+    public GameObject wave;
     public Transform spawnPoint;
     
     private Cube currentCubeScript = null;
 	private GameObject oldCube = null;
 	private ScoreManager scoreManager;
 	private CameraManager cameraManager;
+    private int _frameCount;
 
     void Start()
     {
+        _frameCount = 0;
+
 		scoreManager = GameObject.FindGameObjectWithTag ("Score_Manager").GetComponent<ScoreManager> ();
 		cameraManager = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<CameraManager> ();
 
 		SpawnCube ();
+        SpawnWave();
     }
 
 	void Update () {
+        if (_frameCount % 20 == 0)
+        {
+            Debug.Log("Raising Wave");
+
+            RaiseWave();
+            //check lose condition
+        }
+
 		if (currentCubeScript.CheckWin ()) { /* Cube complete */
 			/* Update score */
 			scoreManager.UpdateScore ();
@@ -35,6 +48,8 @@ public class LevelManager : MonoBehaviour
 			/* Update camera */
 			cameraManager.UpdateCamera ();
 		}
+
+        _frameCount++;
 	}
 	
 	void SpawnCube () {
@@ -51,4 +66,23 @@ public class LevelManager : MonoBehaviour
 		currentCubeScript.enabled = true;
 		oldCube = currentCube;
 	}
+
+    void SpawnWave ()
+    {
+        var wavePosition = new Vector3(
+            spawnPoint.position.x,
+            spawnPoint.position.y - 0.5f,
+            spawnPoint.position.z);
+        wave = Instantiate(wave, wavePosition, spawnPoint.rotation);
+    }
+
+    void RaiseWave ()
+    {
+        var newPosition = new Vector3(
+            wave.transform.position.x,
+            wave.transform.position.y + 0.025f,
+            wave.transform.position.z);
+
+        wave.transform.position = newPosition;
+    }
 }
