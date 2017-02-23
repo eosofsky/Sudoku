2df56ac649a9a763b5ce542a33197b330d896715
clarencenpy <clarencenpy@gmail.com>
2017-02-22 19:16:21 -0500

    made thunder more realistic, with sounds

--- Assets/Scripts/LightningManager.cs
+++ Assets/Scripts/LightningManager.cs
@@ -8,18 +8,44 @@ public class LightningManager : MonoBehaviour {
 	public float minInterval;
 	public float threshold;
 
+	public AudioSource thunder_bg;
+	public AudioSource thunder_1;
+	public AudioSource thunder_2;
+	public AudioSource thunder_3;
+	public AudioSource thunder_4;
+
 	private float lastTime = 0;
+	private AudioSource[] thunderSounds;
+	private bool lightningActive;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		
+		thunderSounds = new AudioSource[] {
+			thunder_1,
+			thunder_2,
+			thunder_3,
+			thunder_4
+		};
+
+		for (int i = 0; i < thunderSounds.Length; i++) {
+			thunderSounds[i].rolloffMode = AudioRolloffMode.Linear;
+		}
+	}
+
+	void Start() {
+		light.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time - lastTime > minInterval) {
+		float currentTime = Time.time;
+		// wait at least 10 seconds before starting lightning
+		if (currentTime > 10 && currentTime - lastTime > minInterval && !lightningActive) {
 			if (Random.value > threshold) {
-				light.enabled = true;
-				light.intensity = 0.5f + (Random.value / 5.0f);
+				StartCoroutine (LightningSequence ());
+				thunderSounds [Random.Range (0, 3)].Play (); // play a random thunder clip
+				StartCoroutine(Delay(Random.value / 2 + 0.5f));
 			} else {
 				light.enabled = false;
 				lastTime = Time.time;
@@ -27,4 +53,24 @@ public class LightningManager : MonoBehaviour {
 		}
 
 	}
+
+	IEnumerator Delay(float time) {
+		lightningActive = true;
+		yield return new WaitForSeconds(time);
+		lightningActive = false;
+	}
+
+	IEnumerator LightningSequence() {
+		int iterations = Random.Range (2, 4);
+
+		for (int i = 0; i <= iterations; i++) {
+			light.intensity = 0.5f + (Random.value / 2.0f);
+			light.enabled = true;
+			yield return new WaitForSeconds(Random.value / 8.0f);
+			light.enabled = false;
+			yield return new WaitForSeconds(Random.value / 6.0f);
+		}
+			
+	}
+
 }
