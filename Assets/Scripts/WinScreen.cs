80d58464ada482e8044ac3b752e8f5dd081cf6f8
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-02-23 22:55:50 -0500

    Added a win screen.

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour {
+
+	//public AnimationClip fadeColorAnimationClip;
+
+	//[HideInInspector] public Animator animColorFade;
+
+	void Awake () {
+		if (CursorManager.instance) {
+			CursorManager.instance.SetIdle ();
+		}
+	}
+
+	//public void LoadDelayedRestart()
+	//{
+	//	SceneManager.LoadScene ("Scene1");
+	//}
+
+	public void Restart () {
+		//Invoke ("LoadDelayedRestart", fadeColorAnimationClip.length * .5f);
+		//animColorFade.SetTrigger ("fade");
+		if (CursorManager.instance) {
+			CursorManager.instance.ClickButtonInMenu ();
+		}
+
+		Application.LoadLevel("Scene1");
+	}
+
+	public void ReturnToMenu () {
+		if (CursorManager.instance) {
+			CursorManager.instance.ClickButtonInMenu ();
+		}
+
+		Application.LoadLevel("menu_Screen");
+	}
+}
